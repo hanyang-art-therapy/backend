025fef17b7394d06947e871dbdc1e3ff6e2830e9
SeoJeongYu <bbwest0709@gmail.com>
2025-05-01 16:38:46 +0900

    [SETTING] 스프링부트 세팅

---
+++ hanyang-arttherapy/src/main/java/com/hanyang/arttherapy/HanyangArttherapyApplication.java
@@ -0,0 +1,13 @@
+package com.hanyang.arttherapy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanyangArttherapyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanyangArttherapyApplication.class, args);
+	}
+
+}
