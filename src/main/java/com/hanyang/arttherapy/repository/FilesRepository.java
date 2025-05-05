56d813ac8cc2910b16a69210c5360ea99c548319
SeoJeongYu <bbwest0709@gmail.com>
2025-05-06 02:55:07 +0900

    [FEAT] 파일 번호와 사용 여부로 조회 메서드 추가

--- src/main/java/com/hanyang/arttherapy/repository/FilesRepository.java
+++ src/main/java/com/hanyang/arttherapy/repository/FilesRepository.java
@@ -1,7 +1,11 @@
 package com.hanyang.arttherapy.repository;
 
+import java.util.*;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.hanyang.arttherapy.domain.Files;
+import com.hanyang.arttherapy.domain.*;
 
-public interface FilesRepository extends JpaRepository<Files, Long> {}
+public interface FilesRepository extends JpaRepository<Files, Long> {
+  Optional<Files> findByFilesNoAndUseYn(Long id, boolean userYn);
+}
