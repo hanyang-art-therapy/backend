fb21601f16291b63c0d346a5a6b857aaf4d81e27
SeoJeongYu <bbwest0709@gmail.com>
2025-05-06 03:01:30 +0900

    [FEAT] 생성일자 기준으로 소프트 삭제된 파일 조회 메서드 추가

--- src/main/java/com/hanyang/arttherapy/repository/FilesRepository.java
+++ src/main/java/com/hanyang/arttherapy/repository/FilesRepository.java
@@ -1,5 +1,6 @@
 package com.hanyang.arttherapy.repository;
 
+import java.time.*;
 import java.util.*;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,4 +9,7 @@ import com.hanyang.arttherapy.domain.*;
 
 public interface FilesRepository extends JpaRepository<Files, Long> {
   Optional<Files> findByFilesNoAndUseYn(Long id, boolean userYn);
+
+  List<Files> findByCreatedAtBeforeAndDelYnAndUseYn(
+      LocalDateTime cutoffDate, boolean delYn, boolean userYn);
 }
