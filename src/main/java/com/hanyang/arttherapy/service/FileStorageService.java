ec5945efa0241560b1fe458fbb7c70faa53960d3
SeoJeongYu <bbwest0709@gmail.com>
2025-05-05 22:16:56 +0900

    [REFACTOR] 파일 업로드 공통 로직 분리

--- src/main/java/com/hanyang/arttherapy/service/FileStorageService.java
+++ src/main/java/com/hanyang/arttherapy/service/FileStorageService.java
@@ -1,6 +1,5 @@
 package com.hanyang.arttherapy.service;
 
-import java.io.*;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -9,9 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.hanyang.arttherapy.common.exception.*;
-import com.hanyang.arttherapy.common.exception.exceptionType.*;
-import com.hanyang.arttherapy.domain.*;
+import com.hanyang.arttherapy.domain.Files;
 import com.hanyang.arttherapy.domain.enums.*;
 import com.hanyang.arttherapy.dto.response.*;
 import com.hanyang.arttherapy.repository.*;
@@ -26,6 +23,7 @@ import lombok.extern.slf4j.Slf4j;
 public class FileStorageService {
 
   private final FilesRepository filesRepository;
+  private final FileStorageUtils fileUtils;
 
   @Value("${app.storage.path}")
   private String storagePath;
@@ -38,12 +36,12 @@ public class FileStorageService {
         files.stream()
             .map(
                 file -> {
-                  String extension = extractExtension(file.getOriginalFilename());
-                  validateFileExtension(type, extension);
-                  String savedName = generateUUIDFileName(extension);
+                  String extension = fileUtils.extractExtension(file.getOriginalFilename());
+                  fileUtils.validateFileExtension(type, extension);
+                  String savedName = fileUtils.generateUUIDFileName(extension);
                   String filePath = type.getFullPath(storagePath, savedName);
 
-                  saveFile(file, filePath);
+                  fileUtils.saveFile(file, filePath);
 
                   Files filesEntity = convertToEntity(file, type, savedName, filePath, extension);
                   filesRepository.save(filesEntity);
@@ -64,35 +62,5 @@ public class FileStorageService {
         .extension(extension)
         .filesType(type)
         .build();
-  }
-
-  // 파일 저장
-  private void saveFile(MultipartFile file, String filePath) {
-    try {
-      file.transferTo(new File(filePath));
-    } catch (IOException e) {
-      throw new CustomException(FileSystemExceptionType.FILE_SAVE_FAILED);
-    }
-  }
-
-  // 확장자 검증
-  private void validateFileExtension(FilesType type, String extension) {
-    if (!type.isAllowed(extension)) {
-      throw new CustomException(FileSystemExceptionType.INVALID_FILE_EXTENSION);
-    }
-  }
-
-  // 확장자 추출
-  private String extractExtension(String originFilename) {
-    int dotIndex = originFilename.lastIndexOf(".");
-    if (dotIndex == -1) {
-      throw new CustomException(FileSystemExceptionType.FILE_EXTENSION_MISSING);
-    }
-    return originFilename.substring(dotIndex + 1);
-  }
-
-  // UUID 파일명 생성
-  private String generateUUIDFileName(String extension) {
-    return UUID.randomUUID().toString() + "." + extension;
   }
 }
