ff8cf4cdb2bf35f51db0aefd8abffc7feb9ca5c5
SeoJeongYu <bbwest0709@gmail.com>
2025-05-06 03:01:53 +0900

    [FEAT] 파일 삭제 메서드 추가

--- src/main/java/com/hanyang/arttherapy/service/FileStorageService.java
+++ src/main/java/com/hanyang/arttherapy/service/FileStorageService.java
@@ -4,6 +4,7 @@ import java.util.*;
 
 import org.springframework.web.multipart.MultipartFile;
 
+import com.hanyang.arttherapy.domain.Files;
 import com.hanyang.arttherapy.domain.enums.*;
 import com.hanyang.arttherapy.dto.response.*;
 
@@ -11,4 +12,6 @@ public interface FileStorageService {
   FileResponseListDto store(List<MultipartFile> files, FilesType type);
 
   void softDeleteFile(Long filesNo);
+
+  void deletedFileFromSystem(Files file);
 }
