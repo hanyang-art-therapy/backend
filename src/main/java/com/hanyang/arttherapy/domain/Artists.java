f97ac719312de46e64d8b6cafc24fa26aae429a4
SeoJeongYu <bbwest0709@gmail.com>
2025-05-07 08:43:58 +0900

    [FEAT] Artists 엔티티 클래스 생성

---
+++ src/main/java/com/hanyang/arttherapy/domain/Artists.java
@@ -0,0 +1,35 @@
+package com.hanyang.arttherapy.domain;
+
+import java.util.Optional;
+
+import jakarta.persistence.*;
+
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Artists {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long artistsNo;
+
+  @Column(nullable = false)
+  private String artistName;
+
+  @Column(unique = true, nullable = false, length = 10)
+  private String studentNo;
+
+  @Column(nullable = false)
+  private int cohort;
+
+  public void updateArtistInfo(
+      Optional<String> artistName, Optional<String> studentNo, Optional<Integer> cohort) {
+    artistName.ifPresent(value -> this.artistName = value);
+    studentNo.ifPresent(value -> this.studentNo = value);
+    cohort.ifPresent(value -> this.cohort = value);
+  }
+}
