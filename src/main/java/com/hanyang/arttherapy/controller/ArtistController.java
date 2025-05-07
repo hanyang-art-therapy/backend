11c5ba5465f02b9b82f4aa59920084bb1d5e559c
SeoJeongYu <bbwest0709@gmail.com>
2025-05-07 12:22:10 +0900

    [FEAT] 작가 전체 조회 API

--- src/main/java/com/hanyang/arttherapy/controller/ArtistController.java
+++ src/main/java/com/hanyang/arttherapy/controller/ArtistController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.*;
 
 import com.hanyang.arttherapy.dto.request.*;
 import com.hanyang.arttherapy.dto.response.ArtistResponseDto;
+import com.hanyang.arttherapy.dto.response.ArtistResponseListDto;
 import com.hanyang.arttherapy.service.ArtistsService;
 
 import lombok.RequiredArgsConstructor;
@@ -25,5 +26,10 @@ public class ArtistController {
   @GetMapping("/{artistsNo}")
   public ResponseEntity<ArtistResponseDto> getArtist(@PathVariable Long artistsNo) {
     return ResponseEntity.ok(artistsService.getArtist(artistsNo));
+  }
+
+  @GetMapping
+  public ResponseEntity<ArtistResponseListDto> getArtists() {
+    return ResponseEntity.ok(artistsService.getArtists());
   }
 }
