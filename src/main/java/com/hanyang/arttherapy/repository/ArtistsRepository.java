9bc3536d738cd76e77deabda5efb6402a94443a8
SeoJeongYu <bbwest0709@gmail.com>
2025-05-07 12:04:30 +0900

    [FEAT] 학번 중복 체크 메서드 추가

--- src/main/java/com/hanyang/arttherapy/repository/ArtistsRepository.java
+++ src/main/java/com/hanyang/arttherapy/repository/ArtistsRepository.java
@@ -4,4 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.hanyang.arttherapy.domain.Artists;
 
-public interface ArtistsRepository extends JpaRepository<Artists, Long> {}
+public interface ArtistsRepository extends JpaRepository<Artists, Long> {
+  boolean existsByStudentNo(String studentNo);
+}
