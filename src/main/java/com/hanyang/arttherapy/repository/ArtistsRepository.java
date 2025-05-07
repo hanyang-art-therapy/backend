7ca202b1b176a3e6dff137f0fae224016e3c8bc7
SeoJeongYu <bbwest0709@gmail.com>
2025-05-07 10:02:35 +0900

    [FEAT] ArtistsRepository 추가

---
+++ src/main/java/com/hanyang/arttherapy/repository/ArtistsRepository.java
@@ -0,0 +1,7 @@
+package com.hanyang.arttherapy.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.hanyang.arttherapy.domain.Artists;
+
+public interface ArtistsRepository extends JpaRepository<Artists, Long> {}
