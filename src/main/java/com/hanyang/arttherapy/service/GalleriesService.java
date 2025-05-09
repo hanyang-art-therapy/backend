37b101280b53e6fbe3deaa72c2a121d539bf4699
yeooneeee <129919629+yeooneeee@users.noreply.github.com>
2025-05-09 13:57:33 +0900

    [FEAT] 전시회 등록, 수정, 삭제 기능 구현

---
+++ src/main/java/com/hanyang/arttherapy/service/GalleriesService.java
@@ -0,0 +1,48 @@
+package com.hanyang.arttherapy.service;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.hanyang.arttherapy.common.exception.CustomException;
+import com.hanyang.arttherapy.common.exception.exceptionType.GalleryExceptionType;
+import com.hanyang.arttherapy.domain.Galleries;
+import com.hanyang.arttherapy.repository.GalleriesRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+@Profile("h2")
+public class GalleriesService {
+
+  private final GalleriesRepository galleriesRepository;
+
+  public Galleries save(Galleries galleries) {
+    return galleriesRepository.save(galleries);
+  }
+
+  public List<Galleries> getAllGalleries() {
+    return galleriesRepository.findAll();
+  }
+
+  public Galleries getGalleryById(Long id) {
+    return galleriesRepository
+        .findById(id)
+        .orElseThrow(() -> new CustomException(GalleryExceptionType.GALLERY_NOT_FOUND));
+  }
+
+  public Galleries update(Long id, Galleries updated) {
+    Galleries gallery = getGalleryById(id);
+    gallery.update(updated.getTitle(), updated.getStartDate(), updated.getEndDate());
+    return galleriesRepository.save(gallery);
+  }
+
+  public void delete(Long id) {
+    Galleries gallery = getGalleryById(id);
+    galleriesRepository.delete(gallery);
+  }
+}
