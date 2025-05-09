7adb33fcc390157b4112c146fd0b8703af2e1042
yeooneeee <129919629+yeooneeee@users.noreply.github.com>
2025-05-09 13:58:57 +0900

    [FEAT] 전시회 예외 처리 유형

---
+++ src/main/java/com/hanyang/arttherapy/common/exception/exceptionType/GalleryExceptionType.java
@@ -0,0 +1,29 @@
+package com.hanyang.arttherapy.common.exception.exceptionType;
+
+import org.springframework.http.HttpStatus;
+
+public enum GalleryExceptionType implements ExceptionType {
+  GALLERY_NOT_FOUND(HttpStatus.NOT_FOUND, "전시회를 찾을 수 없습니다."),
+  GALLERY_CREATE_FAIL(HttpStatus.BAD_REQUEST, "전시회 생성에 실패했습니다."),
+  GALLERY_UPDATE_FAIL(HttpStatus.BAD_REQUEST, "전시회 수정에 실패했습니다."),
+  GALLERY_DELETE_FAIL(HttpStatus.BAD_REQUEST, "전시회 삭제에 실패했습니다."),
+  INVALID_GALLERY_DATE(HttpStatus.BAD_REQUEST, "전시회 날짜가 유효하지 않습니다.");
+
+  private final HttpStatus status;
+  private final String message;
+
+  GalleryExceptionType(HttpStatus status, String message) {
+    this.status = status;
+    this.message = message;
+  }
+
+  @Override
+  public HttpStatus status() {
+    return status;
+  }
+
+  @Override
+  public String message() {
+    return message;
+  }
+}
