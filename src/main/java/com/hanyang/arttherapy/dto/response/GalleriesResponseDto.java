0bf2cd069813e8a1c26cbe2c0693dbc7c4eaeda9
yeooneeee <129919629+yeooneeee@users.noreply.github.com>
2025-05-12 09:41:00 +0900

    [REFACTOR] 전시회 기능 리팩토링

---
+++ src/main/java/com/hanyang/arttherapy/dto/response/GalleriesResponseDto.java
@@ -0,0 +1,27 @@
+package com.hanyang.arttherapy.dto.response;
+
+import java.time.LocalDateTime;
+
+import com.hanyang.arttherapy.domain.Galleries;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class GalleriesResponseDto {
+
+  private final Long galleriesNo;
+  private final String title;
+  private final LocalDateTime startDate;
+  private final LocalDateTime endDate;
+
+  public static GalleriesResponseDto from(Galleries gallery) {
+    return GalleriesResponseDto.builder()
+        .galleriesNo(gallery.getGalleriesNo())
+        .title(gallery.getTitle())
+        .startDate(gallery.getStartDate())
+        .endDate(gallery.getEndDate())
+        .build();
+  }
+}
