a8b0f4de0ce6ca39f943c1ee3565c2db752ca3a9
MoonMinah <mingke48@gmail.com>
2025-05-14 15:07:00 +0900

    Update SecurityConfig.java

--- src/main/java/com/hanyang/arttherapy/common/config/SecurityConfig.java
+++ src/main/java/com/hanyang/arttherapy/common/config/SecurityConfig.java
@@ -45,7 +45,7 @@ public class SecurityConfig {
   @Bean
   public CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration configuration = new CorsConfiguration();
-    configuration.setAllowedOrigins(List.of("https://hy-erica-arttherapy.com"));
+    configuration.setAllowedOrigins(List.of("http://localhost:5173"));
     configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
     configuration.setAllowedHeaders(List.of("*")); // 모든 헤더 허용
     configuration.setAllowCredentials(true); // 인증정보(Cookie 등) 허용
