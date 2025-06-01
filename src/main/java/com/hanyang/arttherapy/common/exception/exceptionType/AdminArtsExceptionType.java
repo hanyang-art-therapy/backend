b52b5dd819eef60a9c514ea1a8fe31b38495ea6d
yeooneeee <129919629+yeooneeee@users.noreply.github.com>
2025-06-01 17:34:42 +0900

    [FEAT] 관리자작품 검색기능
    - 필터[작품명, 작가명] + 검색

--- src/main/java/com/hanyang/arttherapy/common/exception/exceptionType/AdminArtsExceptionType.java
+++ src/main/java/com/hanyang/arttherapy/common/exception/exceptionType/AdminArtsExceptionType.java
@@ -6,7 +6,8 @@ public enum AdminArtsExceptionType implements ExceptionType {
   FILE_NOT_FOUND(HttpStatus.NOT_FOUND, "파일이 존재하지 않습니다."),
   GALLERY_NOT_FOUND(HttpStatus.NOT_FOUND, "전시회가 존재하지 않습니다."),
   ARTIST_NOT_FOUND(HttpStatus.NOT_FOUND, "작가가 존재하지 않습니다."),
-  ARTS_NOT_FOUND(HttpStatus.NOT_FOUND, "작품이 존재하지 않습니다.");
+  ARTS_NOT_FOUND(HttpStatus.NOT_FOUND, "작품이 존재하지 않습니다."),
+  UNAUTHORIZED(HttpStatus.FORBIDDEN, "관리자 권한이 없습니다.");
 
   private final HttpStatus status;
   private final String message;
