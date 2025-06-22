452ed11daf052c4d101ba381e8eed112a068e5de
kyunga1126 <audwk6028@naver.com>
2025-06-22 14:42:15 +0900

    파일 enum 수정

--- src/main/java/com/hanyang/arttherapy/domain/enums/FileExtension.java
+++ src/main/java/com/hanyang/arttherapy/domain/enums/FileExtension.java
@@ -1,13 +1,11 @@
 package com.hanyang.arttherapy.domain.enums;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 import lombok.Getter;
 
 @Getter
-enum FileExtension {
+public enum FileExtension {
   IMAGE(
       "jpg", "jpeg", "png", "gif", "bmp", "tiff", "tif", "webp", "svg", "heif", "heic", "raw",
       "nef", "cr2", "arw", "dng", "orf", "sr2", "ico", "eps", "psd"),
@@ -15,9 +13,9 @@ enum FileExtension {
   VIDEO("mp4", "mkv", "avi", "mov", "flv", "webm"),
   AUDIO("mp3", "wav", "ogg", "flac", "aac", "m4a"),
   ARCHIVE("zip", "rar", "tar", "7z", "gzip"),
-  ALL(combineAll());
+  ALL();
 
-  private final String[] extensions;
+  private String[] extensions;
 
   FileExtension(String... extensions) {
     this.extensions = extensions;
@@ -32,13 +30,13 @@ enum FileExtension {
     return false;
   }
 
-  private static String[] combineAll() {
+  static {
     List<String> all = new ArrayList<>();
     for (FileExtension fe : values()) {
       if (fe != ALL) {
         Collections.addAll(all, fe.extensions);
       }
     }
-    return all.toArray(new String[0]);
+    ALL.extensions = all.toArray(new String[0]);
   }
 }
