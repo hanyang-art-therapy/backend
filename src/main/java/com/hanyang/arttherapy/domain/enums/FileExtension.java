0914800b706c6719d58f878083933978f5bbe75f
kyunga1126 <audwk6028@naver.com>
2025-06-22 14:49:05 +0900

    파일 수정

--- src/main/java/com/hanyang/arttherapy/domain/enums/FileExtension.java
+++ src/main/java/com/hanyang/arttherapy/domain/enums/FileExtension.java
@@ -1,21 +1,16 @@
 package com.hanyang.arttherapy.domain.enums;
 
-import java.util.*;
-
 import lombok.Getter;
 
 @Getter
-public enum FileExtension {
-  IMAGE(
+enum FileExtension {
+  ALL(
       "jpg", "jpeg", "png", "gif", "bmp", "tiff", "tif", "webp", "svg", "heif", "heic", "raw",
-      "nef", "cr2", "arw", "dng", "orf", "sr2", "ico", "eps", "psd"),
-  DOCUMENT("pdf", "docx", "xlsx", "pptx", "txt", "rtf", "odt", "hwp", "hwpx", "ppt", "doc", "xls"),
-  VIDEO("mp4", "mkv", "avi", "mov", "flv", "webm"),
-  AUDIO("mp3", "wav", "ogg", "flac", "aac", "m4a"),
-  ARCHIVE("zip", "rar", "tar", "7z", "gzip"),
-  ALL();
+      "nef", "cr2", "arw", "dng", "orf", "sr2", "ico", "eps", "psd", "pdf", "docx", "xlsx", "pptx",
+      "txt", "rtf", "odt", "hwp", "hwpx", "ppt", "doc", "xls", "mp4", "mkv", "avi", "mov", "flv",
+      "webm", "mp3", "wav", "ogg", "flac", "aac", "m4a", "zip", "rar", "tar", "7z", "gzip");
 
-  private String[] extensions;
+  private final String[] extensions;
 
   FileExtension(String... extensions) {
     this.extensions = extensions;
@@ -28,15 +23,5 @@ public enum FileExtension {
       }
     }
     return false;
-  }
-
-  static {
-    List<String> all = new ArrayList<>();
-    for (FileExtension fe : values()) {
-      if (fe != ALL) {
-        Collections.addAll(all, fe.extensions);
-      }
-    }
-    ALL.extensions = all.toArray(new String[0]);
   }
 }
